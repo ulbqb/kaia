71669cb1b4548528ddefbaae8c709f51eb37b648
Shogo Hyodo <mmoshg8u@gmail.com>
2025-04-08 11:43:14 +0900

    Add wrapping module

--- node/cn/backend.go
+++ node/cn/backend.go
@@ -574,6 +574,8 @@ func (s *CN) SetupKaiaxModules(ctx *node.ServiceContext, mValset valset.ValsetMo
 		return err
 	}
 
+	mTxPool = mBuilder.MakeBuilderWrappingModuleIfNeeded(mTxBundling, mTxPool)
+
 	// Register modules to respective components
 	// TODO-kaiax: Organize below lines.
 	s.RegisterBaseModules(s.stakingModule, mReward, mSupply, s.govModule, mValset, mRandao)
